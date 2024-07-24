edb80e0a85064ae3f72552cf5045182650232db0
Vitor Freitas <vitordeavilafreitas@gmail.com>
2024-07-24 01:53:22 -0300

    feat: tornando a aplicação orientada a objetos, e adicionando funcionalidade de abrir o programa, monitorar o tempo de cpu e analisar o timeout

--- main.py
+++ main.py
@@ -1,33 +1,60 @@
 import psutil
 import win32process
+import win32event
 import time
+import win32api
 
-def monitorar_cpu(pid, time_out):
-    process = psutil.Process(pid)
-    total_cpu_time = 0
+class MonitoringSoftware:
 
-    while (time_out and time_out > 0) or (time_out < 0):
-        cpu_time = process.cpu_times().user
-        total_cpu_time += cpu_time
-        print(f"Tempo de CPU: {cpu_time}")
+    def __init__(self, software, timeout, tempo_cpu):
+        self.software = software
+        self.timeout = timeout
+        self.tempo_cpu = tempo_cpu
+    
+    def monitorar_cpu(self):
+        process_info = win32process.CreateProcess(
+            self.software,
+            None,
+            None,
+            None,
+            False,
+            win32process.CREATE_NEW_CONSOLE,
+            None,
+            None,
+            win32process.STARTUPINFO()
+        )
+        proc_handle, thread_handle, proc_id, thread_id = process_info
+        process = psutil.Process(proc_id)
+        total_cpu_time = 0
+        result = win32event.WaitForSingleObject(proc_handle, self.timeout)
+        while (total_cpu_time < self.tempo_cpu):
+            
+            cpu_time_user = process.cpu_times().user
+            cpu_time_system = process.cpu_times().system
+            total_cpu_time += cpu_time_user+cpu_time_system
+            print(f"Tempo de CPU: {total_cpu_time}")
+            
+            if result == win32event.WAIT_TIMEOUT:
+                print("Timeout expirou. Terminando o processo.")
+                # Termina o processo forçadamente
+                win32api.TerminateProcess(proc_handle, 1)
+                total_cpu_time+=9999999999999999999999
 
-        # Aguarde 1 segundo antes de verificar novamente
-        time.sleep(1)
-        time_out -= 1
+            # Aguarde 1 segundo antes de verificar novamente
+            time.sleep(1)
+            
+        print('Medição encerrada.')
+        win32api.CloseHandle(proc_handle)
+        win32api.CloseHandle(thread_handle)
+        
 
-    print(f"Programa finalizado")
 
-def get_pid_by_name(process_name):
-    for proc in psutil.process_iter(['pid', 'name']):
-        if proc.info['name'] == process_name:
-            return proc.pid
-    return None
+    
 
 if __name__ == "__main__":
-    # Obtenha o PID do Bloco de Notas
-    process_name = str(input("Digite o nome do processo: "))
-    time_out = int(input("Digite um tempo máximo de execução do programa (0 caso não queira tempo máximo): "))
-    pid = get_pid_by_name(process_name + ".exe")
-
-    # Monitore o tempo de CPU do Bloco de Notas
-    monitorar_cpu(pid, time_out - 1)
+    
+    process_name = str(input("Digite o nome do processo: ")) #   Testar com o seguinte diretório: C:\\Windows\\System32\\notepad.exe
+    tempo_cpu = int(input("Digite um tempo máximo de CPU para o programa: "))
+    timeout = int(input("Digite um tempo máximo de execução do programa: "))
+    monitoramento = MonitoringSoftware(process_name, timeout, tempo_cpu)
+    monitoramento.monitorar_cpu()
