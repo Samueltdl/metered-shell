23198190fbfac43b7603e64138ba351221f6e17c
Vitor Freitas <vitordeavilafreitas@gmail.com>
2024-07-29 19:49:46 -0300

    feat: implementando verificações de parada do processo, e reestar do monitoramento em caso de quota de CPU restante.

--- teste.py
+++ teste.py
@@ -1,36 +1,52 @@
 import psutil
-import win32process
 import time
 import subprocess
 
 
 class MonitoringSoftware():
-    def __init__(self, software, timeout): # Inicializa o objeto com o diretório, timeout e tempo de cpu inseridos pelo usuário.
+    def __init__(self, software, timeout, max_cpu): # Inicializa o objeto com o diretório, timeout e tempo de cpu inseridos pelo usuário.
         self.software = software
         self.timeout = timeout
-        #self.tempo_cpu = tempo_cpu
+        self.max_cpu = max_cpu
 
     def abrir_programa(self): # método que abre o programa do caminho especificado
         return subprocess.Popen([self.software])
 
+    def reestart(self):
+        new_timeout = int(input("\nDigite um novo tempo máximo de execução do programa (0 caso não queira tempo máximo): "))
+        self.timeout = new_timeout
+        self.monitorar_cpu()
+        
+    
     def monitorar_cpu(self): # método que roda um loop que fica monitorando o programa
         
         proc = self.abrir_programa()
-
-        time.sleep(2) # sleep adicionado só para dar tempo de o programa iniciar completamente (pode ser pouco tempo se o programa demora para iniciar)
+        print('\n\n...')
+        time.sleep(1)
+        print('....')
+        time.sleep(1)
+        print('.....')
+        time.sleep(1)
+        print('......\n\n')
+        # sleep adicionado só para dar tempo de o programa iniciar completamente (pode ser pouco tempo se o programa demora para iniciar)
 
         process = psutil.Process(proc.pid)
-
-        while process.is_running():
-
-            if self.timeout <= 0: # condição que encerra o programa automaticamente caso tenha atingido o tempo máximo
-                process.terminate()
-
+        
+        time_out = self.timeout
+        if time_out == 0:
+            time_out = True
+        
+        while True:
             if not process.is_running(): # vericica se o programa não foi fechado forçadamente
                 print("\n\n--------------ATENÇÃO---------------")
                 print("\nPrograma encerrado antes de terminar a a medição!")
                 break
 
+            if type(time_out) != bool:
+                if time_out <= 0: # condição que encerra o programa automaticamente caso tenha atingido o tempo máximo
+                    process.terminate()
+                    break
+                
             user_time = process.cpu_times().user
             system_time = process.cpu_times().system
             total_cpu_time = user_time + system_time
@@ -40,21 +56,36 @@ class MonitoringSoftware():
 
             # Aguarde 1 segundo antes de verificar novamente
             time.sleep(1)
-            self.timeout -= 1
+            
+            if type(time_out) != bool:
+                time_out -= 1
 
         print(f'\n\n---------------------------------------------')
-        print(f"\nPrograma finalizado\n\n")
-        print(f"tempo de cpu total consumido: {total_cpu_time}\n\n")
+        print(f"\nPrograma finalizado.\n\n")
+        print(f"Tempo de CPU total consumido: {total_cpu_time}\n\n")
+        
+        if total_cpu_time < self.max_cpu:
+            print(f'Você ainda tem quota de tempo de CPU restante: {self.max_cpu - total_cpu_time}\n')
+            reestart = int(input("\nDeseja reiniciar o monitoramento? (0: não, 1: sim): "))
+            if reestart == 1: 
+                self.max_cpu -= total_cpu_time
+                self.reestart()
+        else:
+            print(f'\nQuota de tempo de CPU excedida em: {total_cpu_time - self.max_cpu}')
+            
 
 
-software = str(input("\n\nDigite o caminho do processo: "))
-timeout = int(input("Digite um tempo máximo de execução do programa (0 caso não queira tempo máximo): "))
+software = str(input("\nDigite o caminho do processo: "))
+timeout = int(input("\nDigite um tempo máximo de execução do programa (0 caso não queira tempo máximo): "))
+max_cpu = int(input("\nDigite uma quota máxima de tempo de CPU para o programa: "))
 
-monitoramento = MonitoringSoftware(software, timeout)
+monitoramento = MonitoringSoftware(software, timeout, max_cpu)
 monitoramento.monitorar_cpu()
 
 
 ################################## ALGUNS DIRETÓRIOS DO MEU PC ###############################
+
 # C:\Program Files\CPUID\HWMonitor\HWMonitor
 # C:\Users\samue\AppData\Local\Discord\app-1.0.9155\Discord
 # C:\Program Files\Windows NT\Accessories\wordpad.exe
+# C:\Program Files (x86)\Steam\steam.exe
