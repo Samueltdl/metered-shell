9d278deec66f9e1da620fdc653c571699ce959d2
Samuel Lemos <samueltdelemos@gmail.com>
2024-07-29 18:31:20 -0300

    chore: adicionando comentários no código

--- teste.py
+++ teste.py
@@ -5,29 +5,28 @@ import subprocess
 
 
 class MonitoringSoftware():
-    def __init__(self, software, timeout): #     Inicializa o objeto com o diretório, timeout e tempo de cpu inseridos pelo usuário.
+    def __init__(self, software, timeout): # Inicializa o objeto com o diretório, timeout e tempo de cpu inseridos pelo usuário.
         self.software = software
-        #self.software_name = software_name
         self.timeout = timeout
         #self.tempo_cpu = tempo_cpu
 
-    def abrir_programa(self):
+    def abrir_programa(self): # método que abre o programa do caminho especificado
         return subprocess.Popen([self.software])
 
-    def monitorar_cpu(self):
+    def monitorar_cpu(self): # método que roda um loop que fica monitorando o programa
         
         proc = self.abrir_programa()
 
-        time.sleep(2)
+        time.sleep(2) # sleep adicionado só para dar tempo de o programa iniciar completamente (pode ser pouco tempo se o programa demora para iniciar)
 
         process = psutil.Process(proc.pid)
 
         while process.is_running():
 
-            if self.timeout <= 0:
+            if self.timeout <= 0: # condição que encerra o programa automaticamente caso tenha atingido o tempo máximo
                 process.terminate()
 
-            if not process.is_running():
+            if not process.is_running(): # vericica se o programa não foi fechado forçadamente
                 print("\n\n--------------ATENÇÃO---------------")
                 print("\nPrograma encerrado antes de terminar a a medição!")
                 break
@@ -35,6 +34,7 @@ class MonitoringSoftware():
             user_time = process.cpu_times().user
             system_time = process.cpu_times().system
             total_cpu_time = user_time + system_time
+            
             print(f"Tempo de CPU (user): {user_time}")
             print(f"Tempo de CPU (system): {system_time}")
 
